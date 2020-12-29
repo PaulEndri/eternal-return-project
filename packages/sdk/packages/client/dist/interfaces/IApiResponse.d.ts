41db926fed7f8e0cd7dcb655dacd493e2f529605
Juan <abrecan.eyndrill@gmail.com>
2020-12-29 00:51:13 -0500

    remove dist

--- packages/sdk/packages/client/dist/interfaces/IApiResponse.d.ts
+++
@@ -1,6 +0,0 @@
-export interface IApiResponse<T = any> {
-    code: number;
-    message: string;
-    data?: T;
-}
-//# sourceMappingURL=IApiResponse.d.ts.map
--- packages/sdk/packages/wikidata/dist/interfaces/IStat.d.ts
+++
@@ -1,7 +0,0 @@
-import { ITiers } from './ITiers';
-export interface IStat {
-    title: string;
-    value: string;
-    tiers: ITiers;
-}
-//# sourceMappingURL=IStat.d.ts.map
