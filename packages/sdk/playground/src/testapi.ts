3301443568223225c8d48b5c2128d6c22cea0a8f
Juan <abrecan.eyndrill@gmail.com>
2020-12-29 01:05:20 -0500

    playground tests

--- packages/sdk/playground/src/testapi.ts
+++ packages/sdk/playground/src/testapi.ts
@@ -5,7 +5,7 @@ const base = 'https://open-api.bser.io';
 const stuff = 'v1/data/hash';
 
 const Headers = {
-	'x-api-key': 'NVstb76ova2O5Jc4hGgpG6zDeXBfaNT45aAj1I5V'
+	'x-api-key': process.env.API_KEY
 };
 
 fetch(`${base}/${stuff}`, { Headers }).then((res) => res.json()).then((data) => {
