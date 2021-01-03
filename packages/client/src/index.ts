91f689f806c731dbba89a45f6e384589487463eb
Juan <abrecan.eyndrill@gmail.com>
2021-01-02 21:15:02 -0500

    updates

--- packages/client/src/index.ts
+++ packages/client/src/index.ts
@@ -7,3 +7,5 @@ export * from './libs/api';
 export * from './constants/GameModes';
 export * from './constants/Masteries';
 export * from './constants/MetaTypes';
+export * from './interfaces/IUserGameHistory';
+export * from './interfaces/IUserRecord';
