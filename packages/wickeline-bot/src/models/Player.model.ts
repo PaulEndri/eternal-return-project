fdb25ebca46f122e1f760261bb41b81287d47f9b
Paul Endri <abrecan.eyndrill@gmail.com>
2021-01-09 20:22:48 -0500

    fix wickeline-bot

--- packages/wickeline-bot/src/models/Player.model.ts
+++ packages/wickeline-bot/src/models/Player.model.ts
@@ -3,39 +3,39 @@ import { IUserRecord } from 'erbs-client/dist/interfaces/IUserRecord';
 import { Document, model, Model, Schema } from 'mongoose';
 
 export interface IPlayerSeasonRecord {
-	season?: number;
-	lastUpdated?: Date;
-	info?: IUserRecord[];
+  season?: number;
+  lastUpdated?: Date;
+  info?: IUserRecord[];
 }
 
 export interface IPlayer extends Document {
-	name: string;
-	id: number;
-	seasonRecords?: IPlayerSeasonRecord[];
-	matches?: IUserGameHistory[];
-	lastUpdated?: Date;
+  name: string;
+  id: number;
+  seasonRecords?: IPlayerSeasonRecord[];
+  matches?: IUserGameHistory[];
+  lastUpdated?: Date;
 }
 
 const PlayerSchema: Schema<IPlayer> = new Schema({
-	name: Schema.Types.String,
-	id: Schema.Types.Number,
-	seasonRecords: [
-		{
-			lastUpdated: Schema.Types.Date,
-			season: Schema.Types.Number,
-			info: Schema.Types.Array
-		}
-	],
-	matches: Schema.Types.Array,
-	lastUpdated: Schema.Types.Date
+  name: Schema.Types.String,
+  id: Schema.Types.Number,
+  seasonRecords: [
+    {
+      lastUpdated: Schema.Types.Date,
+      season: Schema.Types.Number,
+      info: Schema.Types.Array
+    }
+  ],
+  matches: Schema.Types.Array,
+  lastUpdated: Schema.Types.Date
 });
 
-PlayerSchema.pre('save', function(next) {
-	const now = new Date();
+PlayerSchema.pre('save', function (next) {
+  const now = new Date();
 
-	this.lastUpdated = now;
+  this.lastUpdated = now;
 
-	next();
+  next();
 });
 
 export const Players: Model<IPlayer> = model<IPlayer>('Players', PlayerSchema);
--- packages/wickeline-bot/src/services/Client.service.ts
+++ packages/wickeline-bot/src/services/Client.service.ts
@@ -5,101 +5,125 @@ import { Message } from 'discord.js';
 import { IUserRecord } from 'erbs-client/dist/interfaces/IUserRecord';
 
 export const ClientService = {
-	_cacheTime: 1000 * 60 * 60,
-	_client: new ErBsClient(),
-	_limiter: new Bottleneck({ minTime: 1000 / (+process.env.RATE_LIMIT || 1), maxConcurrent: 1 }),
-	getNumber: async (name: string, message: Message): Promise<number> => {
-		const cachedData: IPlayer = await Players.findOne({ name });
+  _cacheTime: 1000 * 60 * 60,
+  _client: new ErBsClient(),
+  _limiter: new Bottleneck({
+    minTime: 1000 / (+process.env.RATE_LIMIT || 1),
+    maxConcurrent: 1
+  }),
+  getNumber: async (name: string, message: Message): Promise<number> => {
+    const cachedData: IPlayer = await Players.findOne({ name });
 
-		if (cachedData) {
-			return cachedData.id;
-		}
+    if (cachedData) {
+      return cachedData.id;
+    }
 
-		if (message) {
-			message.channel.send(
-				"[Update] Searching the Research Center's databases for the requested test subject. I've heard it's British and involves queues."
-			);
-		}
+    if (message) {
+      message.channel.send(
+        "[Update] Searching the Research Center's databases for the requested test subject. I've heard it's British and involves queues."
+      );
+    }
 
-		const results = await ClientService._limiter.schedule(() =>
-			ClientService._client.getPlayerNumber(name)
-		);
+    const results = await ClientService._limiter.schedule(() =>
+      ClientService._client.getPlayerNumber(name)
+    );
 
-		if (results) {
-			await Players.create({
-				name,
-				id: results.userNum
-			});
-		}
+    if (results) {
+      await Players.create({
+        name,
+        id: results.userNum
+      });
+    }
 
-		return results.userNum;
-	},
-	getStats: async (id: number, seasonNumber = 0, message: Message): Promise<IUserRecord[]> => {
-		const cachedData = await Players.findOne({ id });
-		let record: IPlayerSeasonRecord;
+    return results.userNum;
+  },
+  getStats: async (
+    id: number,
+    seasonNumber = 0,
+    message: Message
+  ): Promise<IUserRecord[]> => {
+    const cachedData = await Players.findOne({ id });
+    let record: IPlayerSeasonRecord;
 
-		if (
-			cachedData.seasonRecords &&
-			cachedData.seasonRecords.find(({ season }) => season === seasonNumber)
-		) {
-			record = cachedData.seasonRecords.find(({ season }) => season === seasonNumber);
+    if (
+      cachedData.seasonRecords &&
+      cachedData.seasonRecords.find(({ season }) => season === seasonNumber)
+    ) {
+      record = cachedData.seasonRecords.find(
+        ({ season }) => season === seasonNumber
+      );
 
-			if (new Date().valueOf() - record.lastUpdated.valueOf() < ClientService._cacheTime) {
-				return record.info;
-			}
-		}
+      if (
+        new Date().valueOf() - record.lastUpdated.valueOf() <
+        ClientService._cacheTime
+      ) {
+        return record.info;
+      }
+    }
 
-		message.reply(
-			'[Update] The Research Centered has a lot of data, let me go find it for this unfortunate test subject'
-		);
+    message.reply(
+      '[Update] The Research Centered has a lot of data, let me go find it for this unfortunate test subject'
+    );
 
-		const results = await ClientService._limiter.schedule(() =>
-			ClientService._client.getPlayerRecord(id, seasonNumber)
-		);
+    const results = await ClientService._limiter.schedule(() =>
+      ClientService._client.getPlayerRecord(id, seasonNumber)
+    );
 
-		if (!results) {
-			return null;
-		}
+    if (!results) {
+      return null;
+    }
 
-		if (record) {
-			record.info = results;
-			record.lastUpdated = new Date();
-		} else {
-			if (!cachedData.seasonRecords) {
-				cachedData.seasonRecords = [];
-			}
+    if (record) {
+      record.info = results;
+      record.lastUpdated = new Date();
+    } else {
+      if (!cachedData.seasonRecords) {
+        cachedData.seasonRecords = [];
+      }
 
-			cachedData.seasonRecords.push({
-				season: seasonNumber,
-				lastUpdated: new Date(),
-				info: results
-			});
-		}
+      cachedData.seasonRecords.push({
+        season: seasonNumber,
+        lastUpdated: new Date(),
+        info: results
+      });
+    }
 
-		await Players.findOneAndUpdate({ id }, { seasonRecords: cachedData.seasonRecords });
+    await Players.findOneAndUpdate(
+      { id },
+      { seasonRecords: cachedData.seasonRecords }
+    );
 
-		return results;
-	},
-	getMatches: async (id: number) => {
-		const cachedData: IPlayer = await Players.findOne({ id });
+    return results;
+  },
+  getMatches: async (id: number) => {
+    const cachedData: IPlayer = await Players.findOne({ id });
 
-		if (new Date().valueOf() - cachedData.lastUpdated.valueOf() < ClientService._cacheTime) {
-			return cachedData.matches;
-		}
+    if (
+      new Date().valueOf() - cachedData.lastUpdated.valueOf() <
+      ClientService._cacheTime
+    ) {
+      return cachedData.matches;
+    }
 
-		const limited = ClientService._limiter.wrap(ClientService._client.getGamesForPlayer);
-		const results = await limited(id);
+    const limited = ClientService._limiter.wrap(
+      ClientService._client.getGamesForPlayer
+    );
+    const results = await limited(id);
 
-		results.forEach((match) => {
-			if (cachedData.matches.some((cachedMatch) => cachedMatch.gameId === match.gameId)) {
-				return;
-			}
+    results.forEach((match) => {
+      if (
+        cachedData.matches.some(
+          (cachedMatch) => cachedMatch.gameId === match.gameId
+        )
+      ) {
+        return;
+      }
 
-			cachedData.matches.push(match);
-		});
+      cachedData.matches.push(match);
+    });
 
-		await cachedData.save();
+    await cachedData.save();
 
-		return cachedData.matches;
-	}
+    return cachedData.matches;
+  }
 };
