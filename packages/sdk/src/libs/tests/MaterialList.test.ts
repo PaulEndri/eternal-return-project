c7b61e38f16e3916bb56e783acf7623f83c4ba40
Paul Endri <abrecan.eyndrill@gmail.com>
2021-01-11 10:49:26 -0500

    tests

---
+++ packages/sdk/src/libs/tests/MaterialList.test.ts
@@ -0,0 +1,26 @@
+import { Locations } from 'erbs-data';
+import { Items } from '../../constants';
+import { Location } from '../Location';
+import { MaterialList } from '../MaterialList';
+import { setStaticCache } from '../../utilities/setStaticCache';
+import { Route } from '../Route';
+
+describe('[Class] Material List', () => {
+  beforeAll(() => {
+    setStaticCache();
+  });
+  describe('[iIstance]', () => {
+    describe('getAllCraftableItems()', () => {
+      it('should recursively take into account all possible combinations', () => {
+        const list = new MaterialList();
+        const beach = new Location(Locations.Beach);
+        list.addFromList(beach.materials.list);
+
+        Route.UNIVERSAL_ITEMS.forEach((id) => list.add(id, 10));
+        const results = list.getAllCraftableItems(true);
+
+        expect(results).toContain(Items.MotorcycleHelmet);
+      });
+    });
+  });
+});
